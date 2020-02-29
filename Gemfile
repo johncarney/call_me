74fe177f0192ef92bd7e6afb3572f0c92679930e
John Carney <john@carney.id.au>
2020-02-29 16:50:10 -0700

    Add tests for CallMe.call

--- Gemfile
+++ Gemfile
@@ -5,6 +5,6 @@ source "https://rubygems.org"
 # Specify your gem's dependencies in call_me.gemspec
 gemspec
 
-gem "rake",    "~> 12.0"
-gem "rspec",   "~> 3.0"
-gem "rubocop", "~> 0.80"
+gem "rake", "~> 12.0"
+gem "rspec", "~> 3.0"
+gem "rubocop-rspec", "~> 1.38"
--- spec/call_me_spec.rb
+++ spec/call_me_spec.rb
@@ -5,7 +5,107 @@ RSpec.describe CallMe do
     expect(CallMe::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".call" do
+    let(:test_class) do
+      Class.new do
+        include CallMe
+
+        def call; end
+      end
+    end
+
+    it "creates an instance of the class" do
+      allow(test_class).to receive(:new).and_call_original
+
+      test_class.call
+      expect(test_class).to have_received(:new)
+    end
+
+    it "invokes the instance's #call method" do
+      test_instance = test_class.new
+      allow(test_class).to receive(:new).and_return test_instance
+      allow(test_instance).to receive(:call).and_call_original
+
+      test_class.call
+      expect(test_instance).to have_received(:call)
+    end
+
+    it "returns the result of the instance's #call method" do
+      expected_result = double :result
+      test_instance = instance_double test_class, :test_instance, call: expected_result
+      allow(test_class).to receive(:new).and_return test_instance
+
+      expect(test_class.call).to be expected_result
+    end
+
+    context "with positional arguments" do
+      let(:arguments) { [ double(:first_argument), double(:second_argument) ] }
+
+      it "passes the arguments to the initializer" do
+        test_instance = test_class.new
+        allow(test_class).to receive(:new).and_return test_instance
+
+        test_class.call(*arguments)
+        expect(test_class).to have_received(:new).with(*arguments)
+      end
+
+      it "does not pass the arguments to the instance's #call method" do
+        test_instance = test_class.new
+        allow(test_class).to receive(:new).and_return test_instance
+        allow(test_instance).to receive(:call)
+
+        test_class.call(*arguments)
+        expect(test_instance).to have_received(:call).with no_args
+      end
+    end
+
+    context "with keyword arguments" do
+      let(:arguments) { { first: double(:first_argument), second: double(:second_argument) } }
+
+      it "passes the arguments to the initializer" do
+        test_instance = test_class.new
+        allow(test_class).to receive(:new).and_return test_instance
+
+        test_class.call(**arguments)
+        expect(test_class).to have_received(:new).with(**arguments)
+      end
+
+      it "does not pass the arguments to the instance's #call method" do
+        test_instance = test_class.new
+        allow(test_class).to receive(:new).and_return test_instance
+        allow(test_instance).to receive(:call)
+
+        test_class.call(**arguments)
+        expect(test_instance).to have_received(:call).with no_args
+      end
+    end
+
+    context "with a mix of positional and keyword arguments" do
+      let(:positional_arguments) { [ double(:first_argument), double(:second_argument) ] }
+      let(:keyword_arguments) { { third: double(:third_argument), fourth: double(:fourth_argument) } }
+
+      it "passes the arguments to the initializer" do
+        test_instance = test_class.new
+        allow(test_class).to receive(:new).and_return test_instance
+
+        test_class.call(*positional_arguments, **keyword_arguments)
+        expect(test_class).to have_received(:new).with(*positional_arguments, **keyword_arguments)
+      end
+
+      it "does not pass the arguments to the instance's #call method" do
+        test_instance = test_class.new
+        allow(test_class).to receive(:new).and_return test_instance
+        allow(test_instance).to receive(:call)
+
+        test_class.call(*positional_arguments, **keyword_arguments)
+        expect(test_instance).to have_received(:call).with no_args
+      end
+    end
+  end
+
+  describe '.with' do
+  end
+
+  describe '.[]' do
   end
 end
